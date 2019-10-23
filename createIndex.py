d4d47d2fdce8cfd80edfa7dd3106550d2f507de9
Mehar Madnani <inarutofan1599@gmail.com>
2019-10-23 21:32:27 +0530

    create index file and query

---
+++ createIndex.py
@@ -0,0 +1,85 @@
+
+import re
+import gc
+import json
+from collections import defaultdict
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+
+class Index():
+	"""class for processing and the wikipedia document and storing the index file"""
+	def __init__(self):
+		self.index = defaultdict(list)
+		self.sw = set(stopwords.words('english'))
+		self.collectionFile = 'testCollection.dat'
+		self.indexFile = 'index.json'
+
+	def getTerms(self,line):
+		""" @Input str of words
+			@Output list of stemmed words (excluding stopwords)
+		"""
+		line = line.lower()
+		line = re.sub(r'[^\w\d ]',' ',line)
+		line = line.split(' ')
+		line = [ps.stem(x) for x in line if x not in self.sw]
+		return line
+
+	def getPageInfo(self, file):
+		"""
+			@returns list of dict which contains 'id', 'title', 'text' of pages
+		"""
+		doc = []
+		text = ""
+		for line in open(file,'r'):
+			if line=='</page>\n':
+				pageid=re.search('<id>(.*?)</id>', text, re.DOTALL)
+				pagetitle=re.search('<title>(.*?)</title>', text, re.DOTALL)
+				pagetext=re.search('<text>(.*?)</text>', text, re.DOTALL)
+				d={}
+				d['id']=pageid.group(1)
+				d['title']=pagetitle.group(1)
+				d['text']=pagetext.group(1)
+				doc.append(d)
+				text = ""
+			text += line
+		return doc
+
+	def writeIndexfile(self,file):
+		with open(file, 'w') as fout:
+		    json.dump(self.index, fout)
+
+	def generateIndex(self):
+		"""
+			Generate the indexfile from collection data and saves the term, doc, position info in index file
+		"""
+		gc.disable()
+		# collectionFile = 'testCollection.dat'
+		# indexFile = 'index.json'
+		collectionFile = self.collectionFile
+		indexFile = self.indexFile
+		allPages = self.getPageInfo(collectionFile)
+		for pagedict in allPages:
+			lines='\n'.join((pagedict['title'],pagedict['text']))
+			pageid=int(pagedict['id'])
+			terms=self.getTerms(lines)
+
+			termdictPage={}
+			for position, term in enumerate(terms):
+				try:
+					termdictPage[term][1].append(position)
+				except:
+					termdictPage[term]=[pageid, [position]]
+			for termpage, postingpage in termdictPage.items():
+				self.index[termpage].append(postingpage)
+
+		gc.enable()
+		self.writeIndexfile(indexFile)
+
+
+if __name__ == '__main__':
+	c = Index()
+	c.generateIndex()
+
---
+++ queryIndex.py
@@ -0,0 +1,170 @@
+# import sys
+import re
+import json
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import copy
+from functools import reduce
+
+porter=PorterStemmer()
+
+
+class QueryIndex:
+	"""docstring for QueryIndex"""
+	def __init__(self):
+		self.index = {}
+		self.sw = set(stopwords.words('english'))
+
+	def intersectList(self,lists):
+		if len(lists)==0:
+			return []
+		lists.sort(key=len)
+		return list(reduce(lambda x,y: set(x)&set(y),lists))
+
+	def getTerms(self,line):
+		""" @Input str of words
+			@Output list of stemmed words (excluding stopwords)
+		"""
+		line = line.lower()
+		line = re.sub(r'[^\w\d ]',' ',line)
+		line = line.split()
+		line = [porter.stem(x) for x in line if x not in self.sw]
+		return line
+
+	def getTermInfo(self,terms):
+		"""
+			@returns list of docid, positions of terms from index file
+		"""
+		return [self.index[term] for term in terms]
+
+	def getDocID(self,termInfo):
+		return [ [x[0] for x in p] for p in termInfo ]
+
+
+	def readIndex(self,file):
+		self.index = json.load(open(file,'r'))
+
+
+	def queryType(self,q):
+		"""
+			@returns type of query
+		"""
+		if '"' in q:
+			return "PQ"
+		elif len(q.split())>1:
+			return "FTQ"
+		else:
+			return "OWQ"
+
+	def owq(self,q):
+		"""
+			@returns list of docid for given query
+		"""
+		prevQuery = q
+		q = self.getTerms(q)
+		print("terms ", q)
+		if len(q)==0:
+			print("no terms")
+			return []
+		elif len(q)>1:
+			return self.ftq(prevQuery)
+		q = q[0]
+		if q not in self.index:
+			print("No term found in index file")
+			return []
+		docid = self.index[q]
+		docid = [ x[0] for x in docid]
+		return docid
+
+	def ftq(self,q):
+		"""
+			@returns list of docid for given query
+		"""
+		q = self.getTerms(q)
+		if len(q)==0:
+			print("no term")
+			return []
+		docid = set()
+		for term in q:
+			try:
+				p = self.index[term]
+				p = [ x[0] for x in p]
+				docid = docid|set(p)
+			except Exception as e:
+				pass
+				# raise e
+		return list(docid)
+
+
+	def pq(self,q):
+		"""
+			@returns list of docid for given query
+		"""
+		prevQuery = q
+		q = self.getTerms(q)
+		if len(q)==0:
+			print("no term")
+			return []
+		elif len(q)==1:
+			return self.owq(prevQuery)
+
+		docid = self.pqDoc(q)
+		return docid
+
+	def pqDoc(self,q):
+		"""
+			@returns list of docid for given query
+		"""
+		pharseDoc = []
+		length = len(q)
+		for term in q:
+			if not term in self.index:
+				print("no doc")
+				return []
+
+		postings = self.getTermInfo(q)
+		docs = self.getDocID(postings)
+		docs = self.intersectList(docs)
+
+		for i in range(len(postings)):
+			postings[i] = [x for x in postings[i] if x[0] in docs]
+
+		for i in range(len(postings)):
+			for j in range(len(postings[i])):
+				postings[i][j][1]=[x-i for x in postings[i][j][1]]
+		result = []
+
+		for i in range(len(postings[0])):
+			li = self.intersectList( [ x[i][1] for x in postings ])
+			if len(li)>0:
+				result.append(postings[0][i][0])
+
+		return result
+
+
+	def startquery(self):
+		readFile = 'index.json'
+		self.readIndex(readFile)
+		print("Done reading file ...")
+		while True:
+			q = input()
+			if len(q)<=0:
+				break
+			if q=='quit':
+				break
+			qt = self.queryType(q)
+
+			if qt=="OWQ":
+				docid = self.owq(q)
+				print(docid)
+			elif qt=="FTQ":
+				docid = self.ftq(q)
+				print(docid)
+			elif qt=="PQ":
+				docid = self.pq(q)
+				print(docid)
+			# print("Type ",qt)
+
+if __name__ == '__main__':
+	q = QueryIndex()
+	q.startquery()
